39bc4d00020b5454be1fa42588ff84ca307f691e
135Prithvi <prithvi128717@gmail.com>
2023-07-13 13:48:26 +0530

    ninth commit

--- next.config.js
+++ next.config.js
@@ -2,6 +2,12 @@
 const nextConfig = {
     experimental:{
         serverActions:true
+    },
+    eslint:{
+           ignoreDuringBuilds:true
+    },
+    typescript:{
+        ignoreBuildErrors:true
     }
 }
 
