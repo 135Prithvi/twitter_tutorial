38f71eefca8649042639624913d12cdc47d5f9a9
135Prithvi <prithvi128717@gmail.com>
2023-07-16 12:05:29 +0530

    nineteenth commit

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -7,7 +7,7 @@ export default {
     out: "./drizzle",
     schema: "./app/db/schema.ts",
     dbCredentials: {
-      connectionString: `mysql://${process.env.DATABASE_USERNAME}:${process.env.DATABASE_PASSWORD}@${process.env.DATABASE_HOST}/twitter?ssl={"rejectUnauthorized":true}`
+      connectionString: `mysql://${process.env.DATABASE_USERNAME}:${process.env.DATABASE_PASSWORD}@${process.env.DATABASE_HOST}/twitter-db?ssl={"rejectUnauthorized":true}`
     },
     driver:"mysql2",
     breakpoints: true
